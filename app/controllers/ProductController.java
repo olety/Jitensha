4c76f074b435cf97b80431c378333e98b118cf85
Oleksii Kyrylchuk <olety7@gmail.com>
2017-12-02 17:15:51 +0100

    Controller changes (Auth&Product)
    - Logout should work now
    - Product controller returns the product and the list of compatible
    products

--- app/controllers/ProductController.java
+++ app/controllers/ProductController.java
@@ -8,5 +8,20 @@ import play.data.*;
 import views.html.*;
 import models.*;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static play.mvc.Results.ok;
+
 public class ProductController {
+    public Result getProductList(long productId) {
+        Product resProduct = Product.find.byId(productId);
+        HashSet<Product> compatibleProducts = Compatibility.getCompatibleProducts(productId);
+        Logger.info("Getting details for product {}: \n\tProduct [{}], \n\tCompat [{}]", productId, resProduct, compatibleProducts);
+        return ok(product.render(
+                resProduct,
+                compatibleProducts
+        ));
+    }
+
 }
