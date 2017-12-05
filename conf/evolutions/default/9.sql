0caba13955f8fe655bc3ec3376d942c664e761be
Dominik Kaszewski <dakaszewski@gmail.com>
2017-12-05 11:39:47 +0100

    Added unique key to Baskets table.

---
+++ conf/evolutions/default/9.sql
@@ -0,0 +1,22 @@
+-- Add unique key to Baskets
+
+# --- !Ups
+DROP TABLE Baskets;
+CREATE TABLE Baskets (
+  ID        SERIAL PRIMARY KEY,
+  UserID    INT NOT NULL,
+  ProductID INT NOT NULL,
+  Quantity  INT,
+  CONSTRAINT fk_Baskets_Users FOREIGN KEY (UserID) REFERENCES Users (ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_Baskets_Products FOREIGN KEY (ProductID) REFERENCES Products (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+# --- !Downs
+DROP TABLE Baskets;
+CREATE TABLE Baskets (
+  UserID    INT NOT NULL,
+  ProductID INT NOT NULL,
+  Quantity  INT,
+  CONSTRAINT fk_Baskets_Users FOREIGN KEY (UserID) REFERENCES Users (ID) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_Baskets_Products FOREIGN KEY (ProductID) REFERENCES Products (ID) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
